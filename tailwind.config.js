0a923fb1a172ee26cac9d9bc50e3b22166b60a88
Jonathan Candido <jonathancsrr@gmail.com>
2023-06-04 09:48:12 -0300

    chore: header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      blue: "#0055D1"
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
